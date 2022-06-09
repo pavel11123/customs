618b75d02abb4d05e7869c5a2d08ef85c76bb4fb
pavel11123 <korobko.11@inbox.ru>
2022-06-09 23:47:44 +0600

    first commit

---
+++ main/js/main.js
@@ -0,0 +1,31 @@
+
+function menu__open() {
+   document.getElementById("phone__menu-id").style.width = "100%";
+   document.getElementById("phone__menu-id-body").style.overflow = "hidden";
+}
+
+function menu__close() {
+   document.getElementById("phone__menu-id").style.width = "0";
+   document.getElementById("phone__menu-id-body").style.overflow = "visible";
+}
+
+const popup = () => {
+   const popupBtn = document.getElementById('popup')
+   const popupModal = document.querySelector('.popup')
+
+   const cartCloseBtn = popupModal.querySelector('.cart-close')
+
+   const openPopup = () => {
+      popupModal.style.display = 'flex'
+   }
+
+   const closePopup = () => {
+      popupModal.style.display = 'none'
+   }
+
+   popupBtn.addEventListener('click', openPopup)
+   cartCloseBtn.addEventListener('click', closePopup)
+
+}
+
+popup()
